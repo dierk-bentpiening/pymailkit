f877cd12e14457c08d65a83c909774fc60e73ace
Dierk-Bent Piening <62020056+dierk-bentpiening@users.noreply.github.com>
2023-02-27 11:48:30 +0100

    Bug fixing Template Logic.

--- emailkit/tests/test_emailtemplate.py
+++ emailkit/tests/test_emailtemplate.py
@@ -1,13 +1,13 @@
 import time
-import emailkit.profiler
+import emailkit.tools.profiler
 from emailkit import MailAccount
-from emailkit import MailerDaemon
+from emailkit.email import SimpleMailer
 from emailkit.message import MailMessage
 
 class TestEmailTemplate:
     def test_init_of_template(self):
         account = [MailAccount("test@test.com", "testpw", "smtp.test.com", 667, "SMTP_TLS")]
-        mailer = MailerDaemon(account)
+        mailer = SimpleMailer(account) 
         message = mailer.message_from_template("example")
         assert isinstance(message, MailMessage) is True, "Check if template get generated correctly from templatefile."
         
